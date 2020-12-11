9f9e4b9e60e1b61e222cb61924582128f2b49d43
zxl19 <59674656+zxl19@users.noreply.github.com>
2020-12-11 22:39:00 +0800

    Calibrate Extrinsic Between LiDAR and Camera

---
+++ costFunction_L2C_quat_interp.m
@@ -0,0 +1,25 @@
+function loss = costFunction_L2C_quat_interp(pose_1, pose_2, x0)
+mu = [1, 1, 1e6, 1e6]; % weight
+quat = x0(1, 4 : 7);
+% quat = normalize(quat); % Do Not Use!!!
+quat = quat / sqrt(sum(quat.^2));
+R = quat2rotm(quat);
+t = x0(1, 1 : 3)';
+s = x0(1, 8); % scale
+loss = 0;
+[n, ~] = size(pose_1);
+for i = 2 : n
+    [R_1, t_1] = calcRelativePose_quat(pose_1(i - 1, :), pose_1(i, :));
+    [R_2, t_2] = calcRelativePose_quat(pose_2(i - 1, :), pose_2(i, :));
+%     loss = loss + mu(1) * norm(R_1 * R - R * R_2, 2) + ...
+%         mu(2) * norm(R_1 * t + t_1 * s - R * t_2 - t, 2); % Worse
+%     loss = loss + mu(1) * norm(R_1 * R - R * R_2, 2) + ...
+%         mu(2) * norm(R_1 * t + t_1 * s - R * t_2 - t, 2) + ...
+%         mu(2) * norm(x0(1, 1 : 3) - [0.3, 0.11, -0.85], 2); % Consider Measurement
+    loss = loss + mu(1) * (norm(R_1 * R - R * R_2, 2))^2 + ...
+        mu(2) * (norm(R_1 * t + t_1 - R * t_2 * s - t, 2))^2; % Work
+%     loss = loss + mu(1) * sum(sum((R_1 * R - R * R_2).^2)) + ...
+%         mu(2) * sum(sum((R_1 * t + t_1 * s - R * t_2 - t).^2)); % Test
+end
+% loss = loss + mu(3) * norm(R * R' - eye(3), 2) + mu(4) * norm(R * R' - eye(3), 2); % Not Needed
+end
---
+++ main_calibration_L2C_quat_interp.m
@@ -0,0 +1,116 @@
+close all
+clear
+clc
+format long
+%% Abstract
+% LiDAR Coordinate in A-LOAM:
+% x (back)
+% y (right)
+% z (up)
+% Camera Coordinate
+% x (right)
+% y (down)
+% z (front)
+% INS Coordinate
+% x (right)
+% y (front)
+% z (up)
+%% Pose Filename Setup
+filename_1 = "./data/LO_FCPE.mat"; % LiDAR Odometry
+filename_2 = "./data/VO_FCPE.mat"; % Visual Odometry
+%% Read LiDAR Odometry and INS Data
+data_1 = load(filename_1, '-ascii');
+data_2 = load(filename_2, '-ascii');
+timestamp_1 = data_1(:, 1);
+timestamp_2 = data_2(:, 1);
+pose_1 = data_1(:, 2 : 8);
+pose_2 = data_2(:, 2 : 8);
+%% Coordinate Transformation
+R0_1 = quat2rotm(pose_1(1, 4 : 7)); % qw qx qy qz
+t0_1 = pose_1(1, 1 : 3);
+[m, ~] = size(pose_1);
+for i = 1 : m
+    pose_1(i, 1 : 3) = R0_1 \ (pose_1(i, 1 : 3)' - t0_1');
+    R = R0_1 \ quat2rotm(pose_1(i, 4 : 7)); % qw qx qy qz
+    quat = rotm2quat(R); % qw qx qy qz
+    pose_1(i, 4 : 7) = quat; % qw qx qy qz
+end
+R0_2 = quat2rotm(pose_2(1, 4 : 7));
+t0_2 = pose_2(1, 1 : 3);
+[n, ~] = size(pose_2);
+for i = 1 : n
+    pose_2(i, 1 : 3) = R0_2 \ (pose_2(i, 1 : 3)' - t0_2');
+    R = R0_2 \ quat2rotm(pose_2(i, 4 : 7)); % qw qx qy qz
+    quat = rotm2quat(R); % qw qx qy qz
+    pose_2(i, 4 : 7) = quat; % qw qx qy qz
+end
+%% Pose Interpolation
+[pose_1_interp, timestamp_1_interp, pose_2_interp, timestamp_2_interp] = poseInterp(pose_1, timestamp_1, pose_2, timestamp_2);
+%% Plot to Check Data
+figure
+hold on
+grid on
+axis equal
+plot3(pose_1_interp(:, 1), pose_1_interp(:, 2), pose_1_interp(:, 3), 'bo-', 'LineWidth', 2)
+plot3(pose_2_interp(:, 1), pose_2_interp(:, 2), pose_2_interp(:, 3), 'rs-', 'LineWidth', 2)
+xlabel('X / m')
+ylabel('Y / m')
+zlabel('Z / m')
+title('Before Calibration')
+legend('LiDAR Odometry', 'Visual Odometry')
+%% Optimization
+fun = @(x)costFunction_L2C_quat_interp(pose_1_interp, pose_2_interp, x);
+% options = optimset( 'Display', 'iter', 'MaxFunEvals', 1e6, 'MaxIter', 1e6);
+options = optimset('PlotFcns', 'optimplotfval', 'MaxFunEvals', 1e6, 'MaxIter', 1e6);
+% Constrained
+A = [];
+b = [];
+Aeq = [];
+beq = [];
+lb = [-1, -1, -1, -2, -2, -2, -2, 0];
+ub = [1, 1, 1, 2, 2, 2, 2, 10];
+x0 = (lb + ub) / 2; % x y z (m) qw qx qy qz scale
+x0(1, 4 : 7) = [1, 0, 0, 0]; % qw qx qy qz
+% x0 = [-0.5, -0.3, -0.02, -pi / 2, -pi / 2, 0, 3]; % Measurement: x y z (m) yaw pitch roll (rad) scale
+% Possible Gimbal Lock!
+[x,fval,exitflag,output] = fmincon(fun, x0, A, b, Aeq, beq, lb, ub, [], options); % Constrained
+% x(1, 4 : 7) = normalize(x(1, 4 : 7)); % Do Not Use!!!
+x(1, 4 : 7) = x(1, 4 : 7) / sqrt(sum(x(1, 4 : 7).^2));
+%% Transform
+% R12 = quat2rotm(x(1, 4 : 7));
+% t12 = x(1, 1 : 3);
+fprintf("LiDAR -> Camera Extrinsic: |\tX\t\t|\tY\t\t|\tZ\t\t|\tqw\t\t|\tqx\t\t|\tqy\t\t|\tqz\t\t|\tScale\t|\n")
+fprintf("LiDAR -> Camera Extrinsic: |\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\n", x)
+eul = quat2eul(x(1, 4 : 7), 'ZYX');
+fprintf("LiDAR -> Camera Extrinsic: |\tX\t\t|\tY\t\t|\tZ\t\t|\tYaw\t\t|\tPitch\t|\tRoll\t|\tScale\t|\n")
+fprintf("LiDAR -> Camera Extrinsic: |\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\t%.4f\t|\n", x(1, 1 : 3), eul, x(1, 8))
+scale = x(1, 8);
+T12 = quat2tform(x(1, 4 : 7));
+T12(1 : 3, 4) = x(1, 1 : 3)';
+fprintf("T12 = \n")
+disp(T12)
+fprintf("T12^-1 = \n")
+disp(inv(T12))
+[m, ~] = size(pose_1_interp);
+pose_L2C = zeros(m, 7);
+for i = 1 : m
+    pose_1_temp = quat2tform(pose_1_interp(i, 4 : 7));
+    pose_1_temp(1 : 3, 4) = pose_1_interp(i, 1 : 3)';
+    pose_L2C_temp = T12 \ pose_1_temp * T12; % Correct
+%     pose_L2I_temp = pose_1_temp * T12; % Wrong !!!
+    pose_L2C(i, :) = [pose_L2C_temp(1 : 3, 4)', tform2quat(pose_L2C_temp)];
+end
+pose_2_interp(:, 1 : 3) = pose_2_interp(:, 1 : 3) * scale;
+%% Plot to Check Data
+figure
+hold on
+grid on
+axis equal
+plot3(pose_1_interp(:, 1), pose_1_interp(:, 2), pose_1_interp(:, 3), 'k^-.', 'LineWidth', 1)
+plot3(pose_L2C(:, 1), pose_L2C(:, 2), pose_L2C(:, 3), 'bo-', 'LineWidth', 2)
+plot3(pose_2_interp(:, 1), pose_2_interp(:, 2), pose_2_interp(:, 3), 'rs-', 'LineWidth', 2)
+xlabel('X / m')
+ylabel('Y / m')
+zlabel('Z / m')
+title('After Calibration')
+legend('LiDAR Pose Original', 'LiDAR Pose Transformed', 'Camera with Scale')
